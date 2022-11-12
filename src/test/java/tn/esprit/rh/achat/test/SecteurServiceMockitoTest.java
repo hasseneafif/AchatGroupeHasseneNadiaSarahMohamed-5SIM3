4b3dc98f915f163f24bef8a3b95fab51e331c0a2
sarahbenyahia98 <sarah.benyahia@esprit.tn>
2022-11-12 19:27:29 +0100

    test1

---
+++ src/test/java/tn/esprit/rh/achat/test/SecteurServiceMockitoTest.java
@@ -0,0 +1,14 @@
+package tn.esprit.rh.achat.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SecteurServiceMockitoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
